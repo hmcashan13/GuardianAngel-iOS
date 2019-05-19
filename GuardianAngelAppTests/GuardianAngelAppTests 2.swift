6429e353590170feb9dab12a05bbcbadc2444e70
Hudson <hmcashan13@gmail.com>
2019-05-19 10:41:54 -0500

    changed app icon, converted to swift 5, took out unit tests

--- GuardianAngelAppTests/GuardianAngelAppTests 2.swift
+++ GuardianAngelAppTests/GuardianAngelAppTests 2.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import GuardianAngelApp
 
 class GuardianAngelAppTests: XCTestCase {
 
---
+++ GuardianAngelAppTests/GuardianAngelAppTests 3.swift
@@ -0,0 +1,9 @@
+//
+//  GuardianAngelAppTests.swift
+//  GuardianAngelAppTests
+//
+//  Created by Hudson Mcashan on 5/19/19.
+//  Copyright © 2019 Guardian Angel. All rights reserved.
+//
+
+import Foundation
